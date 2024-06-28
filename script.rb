437a201b96a6081aba4dbc6175ef17a72e93c62f
7minutech <elijohnson1107@gmail.com>
2024-06-27 22:41:11 -0500

    Added method to make hash out of days
    Each day has a index, price, and max profit

---
+++ script.rb
@@ -0,0 +1,16 @@
+
+stocks = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(stocks)
+end
+def make_days_hash(stocks)
+    days = {}
+    stocks.each_with_index do |price,index|
+        days["day#{index+1}".to_sym] = {day_num: index+1, day_price: price, most_day_proft: 0}
+    end
+    days
+end
+p make_days_hash(stocks)
+stock_picker(stocks)
+#expected output
+#1,4]  # for a profit of $15 - $3 == $12
